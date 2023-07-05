d2f5f039c325c9696abef0d437cd17fadf3955d4
Piju <9m2pju@gmail.com>
2023-07-06 01:29:35 +0800

    Boot screen for 9W2RUT

--- uvmod_kitchen/build.bat
+++ uvmod_kitchen/build.bat
@@ -10,8 +10,8 @@ rem python mod_change_contrast.py temp\fw.dec.bin
 rem python mod_mic_gain.py temp\fw.dec.bin
 
 rem python mod_custom_font.py temp\fw.dec.bin
-	python mod_custom_font_DO7OO.py temp\fw.dec.bin
-rem python mod_custom_font_VCR.py temp\fw.dec.bin
+rem	python mod_custom_font_DO7OO.py temp\fw.dec.bin
+	python mod_custom_font_VCR.py temp\fw.dec.bin
 
     python mod_custom_freq_ranges.py temp\fw.dec.bin
 rem python mod_custom_noaa_freqs.py temp\fw.dec.bin
@@ -27,5 +27,5 @@ rem python mod_menu_strings.py temp\fw.dec.bin
 rem python mod_custom_bootscreen.py temp\fw.dec.bin
 :: end of mods
 
-python qsfirm.py pack temp\fw.dec.bin temp\fw.ver.bin k5_v2.01.26_9M2TPT.bin
+python qsfirm.py pack temp\fw.dec.bin temp\fw.ver.bin k5_v2.01.26_9W2RUT.bin
 
